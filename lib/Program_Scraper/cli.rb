bf5477b7ddabbc17510808641d6efb62ff136255
taisaoko <ttd3calvin@gmail.com>
2018-11-14 03:20:37 +0000

    Build CLI interface

--- lib/Program_Scraper/cli.rb
+++ lib/Program_Scraper/cli.rb
@@ -2,6 +2,35 @@
 class ProgramScraper::CLI
   
   def call
-    puts "Today's Online Programs:"
+    
+    list_programs
+    detail
   end
+  
+  def list_programs
+    # get programs
+    # Here Doc
+    puts "Today's One-Year Certificate Programs:"
+    puts <<-DOC.gsub /^\s*/,''
+    
+    1. Business Management
+    2. Business Transfer
+    3. Corrections
+    4. Health Information Management Technician
+    5. Law Enforcement
+    6. Supervisory Skills
+    7. Supply Chain Management
+    DOC
+  end
+    
+  def detail
+    puts "Enter the number of the program you'd like more info on or type exit to enter"
+    input = gets.strip
+    case input
+      when "1"
+        puts "More info on program 1 ..."
+      when "2"
+        puts "More info on program 2 ..."
+  end
+    
 end
